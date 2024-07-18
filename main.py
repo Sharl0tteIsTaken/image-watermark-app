f16657907032278f53bfe816cd77b3b020b2d39c
Sharl0tteIsTaken <jamesly2013@gmail.com>
2024-07-18 18:44:41 +0800

    make file structure bearable for human eye, attempt to do a default image before image was load but it won't show.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 from PIL import ImageTk, Image, ImageDraw
 
 # default key dimensions
-window_width = 1000
+window_width = 1200
 window_height = 700
 canvas_width = 860
 canvas_height = 660
@@ -13,38 +13,62 @@ canvas_padx = 30
 canvas_pady = 30
 mark_width = 50
 mark_height = 50
-
-
+default_font = ("Ariel", 20, "normal") #
 
 class WaterMarker():
     def __init__(self) -> None:
         self.window = tk.Tk()
         self.window.title("💧MarkIt.")
-        self.window.minsize(width=window_width, height=window_height)
-        self.window.resizable(width=False, height=False)
+        self.window.geometry(f"{window_width}x{window_height}")
+        # self.window.resizable(width=False, height=False)
+        self.window.option_add("*Font", default_font)
+        self.window.config(background="white")
+        
+        self.setup_labelframe()
+        self.setup_widget()
 
-        block_image = tk.LabelFrame(self.window, bg="brown")
-        block_image.config()
-        block_image.pack()
+    def setup_labelframe(self):
+        self.block_image = tk.LabelFrame(self.window, bg="white")
+        self.block_image.config(border=0)
+        self.block_image.grid(column=0, row=1, ipadx=10, ipady=10)
+        
+        self.block_panel = tk.LabelFrame(self.window, bg="white", text="open file")
+        self.block_panel.grid(column=0, row=0, columnspan=1)
+    
+    def setup_widget(self):
+        self.canvas = tk.Canvas(self.block_image, bg='white', width=canvas_width, height=canvas_height)
+        self.canvas.pack()
+        
+        self.btn_open_image = tk.Button(self.block_panel, text="image", command=self.load_image)
+        self.btn_open_image.grid(column=0, row=0)
+        
+        self.btn_open_mark = tk.Button(self.block_panel, text="watermark", command=self.load_mark)
+        self.btn_open_mark.grid(column=1, row=0)
 
-        # TODO: make this into a function bind with a button
-        image_pil = Image.open('grey_box.png')
+        image_pil = Image.open('assets/img/default_image.png')
+        image_resize = image_pil.resize(self.size_calculate(image_pil.width, image_pil.height, type='image'))
+        default_image = ImageTk.PhotoImage(image_resize)
+        self.canvas.create_image(canvas_width/2, canvas_height/2, image=default_image, anchor='center')
+        # self.canvas_image_default = 
+
+    def load_image(self):
+        
+        image_pil = Image.open('assets/img/grey_box.png')
+        # image_pil = Image.open(filedialog.askopenfilename())
         image_resize = image_pil.resize(self.size_calculate(image_pil.width, image_pil.height, type='image'))
         self.image = ImageTk.PhotoImage(image_resize)
         
+        # calculate where datum's(top left corner of image) position will be in the canvas.
         self.image_datum_x = math.floor((canvas_width - self.image.width()) / 2)
         self.image_datum_y = math.floor((canvas_height - self.image.height()) / 2)
         
-        self.canvas = tk.Canvas(block_image, bg='white', width=canvas_width, height=canvas_height, border=0)
+        # create image in canvas position at datum.
         self.canvas.create_image(self.image_datum_x, self.image_datum_y, image=self.image, anchor='nw')
-        self.canvas.pack()
-
-        self.canvas.bind("<Button-1>", lambda event: self.canvas_action(event, method='clicked'))
-        self.canvas.bind("<Motion>", lambda event: self.canvas_action(event, method='motion'))
-        self.canvas.focus_set()
-        
-        # TODO: make this into a function: load_image() -> watermark_in_canvas() bind with a button
-        mark_pil = Image.open('watermark.png')
+                
+    def load_mark(self):
+        # load watermark
+        mark_pil = Image.open('assets/img/watermark.png')
+        # mark_pil = Image.open(filedialog.askopenfilename())
         mark_resize = mark_pil.resize(self.size_calculate(mark_pil.width, mark_pil.height, type='mark'))
         self.mark = ImageTk.PhotoImage(mark_resize)
         self.mark_offset_x_min = math.floor(self.mark.width() / 2)
@@ -52,11 +76,16 @@ class WaterMarker():
         self.mark_offset_x_max = self.mark.width() - self.mark_offset_x_min
         self.mark_offset_y_max = self.mark.height() - self.mark_offset_y_min
 
-        # TODO: make this into a function: load_image() -> preview_in_canvas()
-        ghost_pil = Image.open('watermark.png')
+        # set preview of watermark
+        ghost_pil = Image.open('assets/img/watermark.png')
         ghost_resize = ghost_pil.resize(self.size_calculate(ghost_pil.width, ghost_pil.height, type='mark'))
         ghost_resize.putalpha(128)
         self.ghost = ImageTk.PhotoImage(ghost_resize)
+        
+        # bind actions on canvas to function.
+        self.canvas.bind("<Button-1>", lambda event: self.canvas_action(event, method='clicked'))
+        self.canvas.bind("<Motion>", lambda event: self.canvas_action(event, method='motion'))
+        self.canvas.focus_set()
 
     # key functions
     def operate(self):
@@ -124,7 +153,6 @@ class WaterMarker():
                 print("first time only AttributeError, no worries.")
             finally:
                 self.watermark_preview = self.canvas.create_image(x, y, image=self.ghost, anchor='nw')
-        msg = f"hover over: x:{x}, y:{y} in canvas."
         
             
 wm = WaterMarker()
